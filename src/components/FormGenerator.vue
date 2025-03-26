fc98202043b96a819c1305c4dea382d7a7cfadb8
Alexander Chernakov <a.chernakov@meta.studio>
2025-03-26 19:59:55 +0300

    feat(FormGenerator): create form generator component

--- src/components/FormGenerator.vue
+++ src/components/FormGenerator.vue
@@ -5,48 +5,89 @@
       :key="field.name"
       class="form__element"
     >
-      <component
-        v-model="modelValue[field.name]"
-        :is="getComponentName(field.type)"
-        :field="field"
-      />
-
-      {{$emit('update:modelValue')}}
+      <slot :name="`field-${field.name}`">
+        <component
+            v-model="model[field.name]"
+            :is="getComponentName(field.type)"
+            :field="field"
+        />
+      </slot>
     </div>
 
     <div class="form__control">
-      <BaseButton>Подтвердить</BaseButton>
-      <BaseButton>Отклонить</BaseButton>
+      <BaseButton class="button--submit" :type="ButtonEnum.submit">Подтвердить</BaseButton>
+      <BaseButton class="button--decline" :type="ButtonEnum.default" @click.prevent="handleClearForm">Отклонить</BaseButton>
     </div>
   </form>
 </template>
 
 <script setup lang="ts">
 import type { FormSettings } from "../global/types/FormSettings.ts";
+import type { FormTypes } from "../global/types/FormTypes.ts";
+import { ButtonEnum } from "../global/enums/ButtonEnum.ts";
 import BaseButton from "./base/BaseButton.vue";
 import BaseInput from "./base/BaseInput.vue";
 import BaseCheckbox from "./base/BaseCheckbox.vue";
 import BaseTextarea from "./base/BaseTextarea.vue";
+import BaseSelect from "./base/BaseSelect.vue";
+import { ref, watch } from "vue";
+import { useStore } from "vuex"
+import { useHandleForm } from "../composables/useHandleForm.ts";
 
 interface Props {
   settings: FormSettings
+  values?: Record<string, string | number | boolean>
+  formType: string
 }
 
-withDefaults(defineProps<Props>(), {
-  settings: [],
-})
+const model = ref<Record<string, any>>({})
+const props = defineProps<Props>()
+const store = useStore()
+const { useFormClear } = useHandleForm(store)
 
-const getComponentName = (type: string) => {
+const getComponentName = (type: FormTypes['type']) => {
   const componentNames = {
-    input: BaseInput,
-    checkbox: 'BaseCheckbox',
-    textarea: 'BaseTextarea',
+    text: BaseInput,
+    password: BaseInput,
+    checkbox: BaseCheckbox,
+    textarea: BaseTextarea,
+    select: BaseSelect,
   }
 
   return componentNames[type]
 }
 
-const model = defineModel({ default: () => ({}) });
+const handlePropModelValues = () => {
+  const settingFields = props.settings?.fields
+  const values = props.values
+
+
+  if (!settingFields || !values || Object.keys(values).length === 0) return
+
+  settingFields.forEach((field: { name: string }) => {
+    if (!props.values) return
+
+    model.value[field.name] = values[field.name]
+  })
+}
+
+const handleSubmit = () => {
+  store.dispatch('forms/saveFormData', {
+    formName: props.formType,
+    data: model.value,
+  })
+}
+
+const handleClearForm = () => {
+  model.value = {}
+  useFormClear(props.formType)
+}
+
+watch(() => props.values, (newValues) => {
+  if (newValues) {
+    handlePropModelValues()
+  }
+}, { deep: true })
 </script>
 
 <style scoped lang="scss">
@@ -54,7 +95,6 @@ const model = defineModel({ default: () => ({}) });
   display: flex;
   flex-direction: column;
   gap: 12px;
-  width: 300px;
 
   &__control {
     display: flex;
