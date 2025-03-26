fcc7a77688463e5ddd66e871bd2a76f9155e1a01
Alexander Chernakov <a.chernakov@meta.studio>
2025-03-26 19:55:44 +0300

    feat(BaseButton): modernized button by add styles and script strings

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,11 +1,39 @@
 <template>
-  <button type="button" class="button">
+  <button :type="type" class="button">
     <slot/>
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ButtonEnum } from "../../global/enums/ButtonEnum.ts";
+
+interface Props {
+  type: ButtonEnum.default | ButtonEnum.submit
+}
+
+defineProps<Props>()
+</script>
 
 <style scoped lang="scss">
+.button {
+  border: 1px solid transparent;
 
+  @media screen and (hover: hover) {
+    &:hover {
+      border-color: #ffffff;
+    }
+  }
+
+  &:active {
+    border-color: #ffffff;
+  }
+
+  &--submit {
+    background: #2b4a2e;
+  }
+
+  &--decline {
+    background: #592b2b;
+  }
+}
 </style>
