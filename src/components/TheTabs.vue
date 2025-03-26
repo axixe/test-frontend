a4c4e312740f70e423a62778066f22a48153b47c
Alexander Chernakov <a.chernakov@meta.studio>
2025-03-26 19:59:04 +0300

    feat(TheTabs): create tabs component

---
+++ src/components/TheTabs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tab-list">
+    <RouterLink
+        v-for="({ numbering, link }, index) in pages"
+        :to="`/${link}`"
+        :key="index"
+    >
+      <BaseButton :type="ButtonEnum.default">{{ numbering }} страница</BaseButton>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseButton from "./base/BaseButton.vue";
+import { ButtonEnum } from "../global/enums/ButtonEnum.ts";
+
+const pages = [
+  {
+    numbering: 'Первая',
+    link: ''
+  },
+  {
+    numbering: 'Вторая',
+    link: 'second'
+  },
+  {
+    numbering: 'Третья',
+    link: 'third'
+  },
+]
+</script>
+
+<style scoped lang="scss">
+.tab-list {
+  display: flex;
+  gap: 32px;
+}
+</style>
